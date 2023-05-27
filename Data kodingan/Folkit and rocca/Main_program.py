ab10a66014429982d5ad8ec442e9922f102aa8d3
Muhammad Faisal Arnur <127120591+baksobundar@users.noreply.github.com>
2023-05-27 10:15:32 +0700

    Add files via upload

---
+++ Data kodingan/Folkit and rocca/Main_program.py
@@ -0,0 +1,10 @@
+import pygame
+import os
+pygame.init()
+from Obstacle_Score_powerup import *
+from Gambar      import *  
+from Tombol      import *
+from Karakter    import * 
+if __name__ == "__main__":
+    start(0)
+    os.system('cls')
